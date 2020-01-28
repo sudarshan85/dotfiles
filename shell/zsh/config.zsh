3408fe0ed75a8ce5466a7aee6531603b54a46356
Sudarshan <sudarshan85@gmail.com>
2020-01-28 21:51:09 +0000

    Added conda site-packages path

--- shell/zsh/config.zsh
+++ shell/zsh/config.zsh
@@ -101,4 +101,21 @@ source $ZSH/oh-my-zsh.sh
  source ~/.dotfiles/shell/zsh/z.sh
  source ~/.dotfiles/shell/aliases
 
+ export SITE_PACK=/lib/python3.7/site-packages
  export PATH=~/miniconda3/bin:$PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/sudarshan/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/sudarshan/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/sudarshan/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/sudarshan/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
